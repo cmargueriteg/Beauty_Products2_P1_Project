66d244c2c6ea96486b57b79aa3f7f1eac9ee8776
Cindy Germain <cindymarguerite@gmail.com>
2022-05-13 17:04:18 -0600

    Started over with new repo

---
+++ index.js
@@ -0,0 +1,50 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    getPolish()
+    
+    
+ })
+ 
+ 
+ function getPolish(){
+     let main = document.getElementById('main ul')
+     let brandList = document.getElementById('brand-list')
+ 
+   fetch(`https://makeup-api.herokuapp.com/api/v1/products.json?product_type=nail_polish`)
+   .then(resp => resp.json())
+   .then(polishes => {
+       polishes.map(polish => {
+          
+            const li = document.createElement("li")
+            const a = document.createElement("a")
+            a.dataset.id= polish.id
+            a.href="#"
+            a.textContent = polish.brand
+            a.addEventListener('click', (e) => bottleCard(e,polish))
+            li.append(a)
+            brandList.append(li)
+ 
+       })
+ 
+   })
+ 
+ }
+   
+ 
+ function bottleCard(e,polish){
+     e.preventDefault()
+     
+     let card = document.getElementById('info-card')
+     card.innerHTML=""
+     
+        card.innerHTML += `
+     <div class="card">
+     <h2>${polish.name}</h2>
+     <img src="${polish.image_link}" class="bottle-image" />
+     <p>${polish.price}</p>
+     <p>${polish.description}</p>
+     
+   </div>
+  `
+     }
+ 
